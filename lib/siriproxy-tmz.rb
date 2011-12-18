cf9238a5c1a96d1c2263fdc68418134d3cd9fd46
Vince Davis <vince.davis@me.com>
2011-12-18 02:03:44 -0600

    first commit

---
+++ lib/siriproxy-tmz.rb
@@ -0,0 +1,49 @@
+require 'cora'
+require 'siri_objects'
+require 'open-uri'
+require 'nokogiri'
+
+#############
+# This is a plugin for SiriProxy that will allow you to check tonight's NFL scores
+# Example usage: "What's the score of the Bears game?"
+#############
+
+class SiriProxy::Plugin::TMZ < SiriProxy::Plugin
+
+	def initialize(config)
+    #if you have custom configuration options, process them here!
+    end
+  
+  listen_for /TMZ/i do |phrase|
+	  tmzNews = "today"
+	  tmz(tmzNews) #in the function, request_completed will be called when the thread is finished
+	end
+	
+	def tmz(news)
+	  Thread.new {
+	    doc = Nokogiri::HTML(open("http://www.tmz.com/rss.xml"))
+      	entry = doc.css(".entry")
+      	entry.each {
+      		|article|
+      		title = article.css("h3 a").first.content.strip
+      		say title
+      		response = ask "Would you like to hear more stories?" #ask the user for something
+    
+    		if(response =~ /yes/i) #process their response
+      			say "OK"
+    		else
+      			say "OK, I'll stop with all the juicy TMZ gossip."
+      			break
+      			request_completed
+    		end
+      		
+      	} 
+			
+			request_completed
+	  }
+		
+	  say "Checking to see if there is any gossip today..."
+	  
+	end
+	
+end
