72867233b208fb73051481d754c495167f36e0bc
Vince Davis <vince.davis@me.com>
2011-12-30 20:11:14 -0600

    Add extra logic

--- lib/siriproxy-tmz.rb
+++ lib/siriproxy-tmz.rb
@@ -10,7 +10,8 @@ require 'nokogiri'
 
 class SiriProxy::Plugin::TMZ < SiriProxy::Plugin
 
-	#@searched = 0 
+	@searched = 0 
+	@articles = []
 	
 	def initialize(config)
     #if you have custom configuration options, process them here!
@@ -24,46 +25,62 @@ class SiriProxy::Plugin::TMZ < SiriProxy::Plugin
 	def tmz(news)
 	  Thread.new {
 	  
-	    
-	    doc = Nokogiri::HTML(open("http://m.tmz.com/home.ftl"))
-      	main = doc.css("div.main_art")
-      	##entry.each {
-      		#|article|
+	    doc = Nokogiri::HTML(open("http://www.tmz.com/"))
+      	entry = doc.css(".post")
+      	
+      	i = 0
+      	
+      	entry.each {
+      		|article|
       		#@searched = 1
-      		title = main.css("span").first.content.strip
-      		img = main.css("a img.img_thumb").first
+      		title = main.css("a span").first.content.strip
+      		img = main.css("p img").first
       		img_url = img['src']
-      		descr = main.css("div").last.content.strip
-      		
-      		say "Here is the latest from TMZ...", spoken: "Here is the latest from TMZ. " + title + "."
-      		
-      		object = SiriAddViews.new
-    		object.make_root(last_ref_id)
-    		answer = SiriAnswer.new(title, [
-      		SiriAnswerLine.new('logo',img_url), # this just makes things looks nice, but is obviously specific to my username
-      		SiriAnswerLine.new(descr)])
-    		object.views << SiriAnswerSnippet.new([answer])
-    		send_object object
-      		
-      		#response = ask "Would you like to hear more gossip?" #ask the user for something
-    
-    		#if(response == /yes/i)- #process their response
-      		#	say "OK, well you w,,=/.,mgfdssss hill have to wait until the plugin is updated. Stay tuned."
-    		#else
-      			#say "OK, I'll stop with all the juicy TMZ gossip."
-      			##break
-      			##request_completed
-    		#end
-      		
-      	#} 
-      		#if @searched == 0
-				#say "I'm sorry, I didn't see any juicy TMZ gossip. I failed you."
-			#end
+      		descr = entry.css(".home-post-text").first.content.strip
+      		articles[i] = [title,img_url,descr]
+      		i = i + 1
+      	}
+      		 
+      	if i == 0
+			say "I'm sorry, I didn't see any juicy TMZ gossip. I failed you."
 			request_completed
+		else
+			showArticle(0)
 	  }
 		
 	  say "Checking to see if there is any gossip today..."
 	  
+	end
+	
+	def showArticle(art)
+		
+		array[] = @articles[art]
+		
+		title = array[0]
+		img_url = array[1]
+		descr = array[2]
+		
+		say "Here is the latest from TMZ...", spoken: "Here is the latest from TMZ. " + title + "."
+		
+		object = SiriAddViews.new
+    	object.make_root(last_ref_id)
+    	answer = SiriAnswer.new(title, [
+      	SiriAnswerLine.new('logo',img_url), # this just makes things looks nice, but is obviously specific to my username
+      	SiriAnswerLine.new(descr)])
+    	object.views << SiriAnswerSnippet.new([answer])
+    	send_object object
+    	
+    	response = ask "Would you like to hear more gossip?" #ask the user for something
+    
+    	if(response =~ /yes/i)- #process their response
+      		@searched = @searched + 1
+      		showArticle(@searched)
+    	else
+      		say "OK, I'll stop with all the juicy TMZ gossip."
+      			#break
+      			request_completed
+    	end
+	
 	end
 	
 end
